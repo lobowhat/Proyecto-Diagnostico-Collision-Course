27d420a7feeb49a3c0a696c14f843c2fccc1f366
howar <howar@howar-HP-G42-Notebook-PC>
2013-02-14 21:25:22 -0600

    jj88

---
+++ Collision Course/estructure.cpp
@@ -0,0 +1,18 @@
+/*
+ * estructure.cpp
+ *
+ *  Created on: 14/02/2013
+ *      Author: howar
+ */
+
+#include "estructure.h"
+
+estructure::estructure() {
+	// TODO Auto-generated constructor stub
+
+}
+
+estructure::~estructure() {
+	// TODO Auto-generated destructor stub
+}
+
