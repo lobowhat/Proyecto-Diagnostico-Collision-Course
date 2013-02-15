416586ffa2d49db036852249bb2932bfe6668d85
Jose Daniel <josebaduma@gmail.com>
2013-02-14 21:57:05 -0600

    Clase nueva

---
+++ git@github.com:lobowhat/proyecto.git/prueba/srcs/Queue.cpp
@@ -0,0 +1,18 @@
+/*
+ * Queue.cpp
+ *
+ *  Created on: 14/02/2013
+ *      Author: jose
+ */
+
+#include "Queue.h"
+
+Queue::Queue() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Queue::~Queue() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ git@github.com:lobowhat/proyecto.git/prueba/srcs/Queue.h
@@ -0,0 +1,17 @@
+/*
+ * Queue.h
+ *
+ *  Created on: 14/02/2013
+ *      Author: jose
+ */
+
+#ifndef QUEUE_H_
+#define QUEUE_H_
+
+class Queue {
+public:
+	Queue();
+	virtual ~Queue();
+};
+
+#endif /* QUEUE_H_ */
