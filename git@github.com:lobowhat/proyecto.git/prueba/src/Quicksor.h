9e5f12a545212503609034e89afcc241e65d6810
howar <howar@howar-HP-G42-Notebook-PC>
2013-02-14 20:45:17 -0600

    jhhh

---
+++ git@github.com:lobowhat/proyecto.git/prueba/src/Quicksor.h
@@ -0,0 +1,17 @@
+/*
+ * Quicksor.h
+ *
+ *  Created on: 14/02/2013
+ *      Author: howar
+ */
+
+#ifndef QUICKSOR_H_
+#define QUICKSOR_H_
+
+class Quicksor {
+public:
+	Quicksor();
+	virtual ~Quicksor();
+};
+
+#endif /* QUICKSOR_H_ */
