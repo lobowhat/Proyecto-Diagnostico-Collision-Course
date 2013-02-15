1c7ab41b01ca3d054e70035248a27af966bd1241
howar <howar@howar-HP-G42-Notebook-PC>
2013-02-14 20:54:56 -0600

    ña

---
+++ git@github.com:lobowhat/proyecto.git/prueba/src/ojo.cpp
@@ -0,0 +1,18 @@
+/*
+ * ojo.cpp
+ *
+ *  Created on: 14/02/2013
+ *      Author: howar
+ */
+
+#include "ojo.h"
+
+ojo::ojo() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ojo::~ojo() {
+	// TODO Auto-generated destructor stub
+}
+
