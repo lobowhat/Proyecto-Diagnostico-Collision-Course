23f06ecba23a9d81e8e0830f43e58adbb41bd084
Jose Daniel <josebaduma@gmail.com>
2013-03-01 17:17:17 -0600

    ejemplo

--- git@github.com:lobowhat/proyecto.git/prueba/Debug/srcs/subdir.mk
+++ git@github.com:lobowhat/proyecto.git/prueba/Debug/srcs/subdir.mk
@@ -3,22 +3,6 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
-CPP_SRCS += \
-../srcs/main.cpp 
-
-OBJS += \
-./srcs/main.o 
-
-CPP_DEPS += \
-./srcs/main.d 
-
 
 # Each subdirectory must supply rules for building sources it contributes
-srcs/%.o: ../srcs/%.cpp
-	@echo 'Building file: $<'
-	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
 
--- git@github.com:lobowhat/proyecto.git/prueba/ejemplos/main.cpp
+++ git@github.com:lobowhat/proyecto.git/prueba/ejemplos/main.cpp
@@ -16,8 +16,8 @@ int sec = 0;
 char exit, oldalchar;
 
 void moveX();
-void scoreDraw();
-void screenDraw(int beginX, int maxX, int maxY);
+void scoreDraw(int beginX, int beginY, int maxX, int maxY);
+void screenDraw(int beginX, int beginY, int maxX, int maxY);
 void initColor();
 
 int main(int argc, char **argv) {
@@ -32,21 +32,142 @@ int main(int argc, char **argv) {
 	oldalchar = (char) 219;
 	initColor();
 	attron(COLOR_PAIR(1));
-	scoreDraw();
-	screenDraw(10, 19, 4);
-	screenDraw(20, 29, 4);
-	screenDraw(30, 39, 4);
-	screenDraw(40, 49, 4);
-	screenDraw(50, 59, 4);
-	screenDraw(60, 69, 4);
-	screenDraw(70, 79, 4);
-	screenDraw(80, 89, 4);
-	screenDraw(90, 99, 4);
-	screenDraw(100, 109, 4);
+	scoreDraw(0, 0, 14, 15);
+	scoreDraw(135, 0, 150, 15);
+	screenDraw(15, 0, 24, 4);
+	screenDraw(25, 0, 34, 4);
+	screenDraw(35, 0, 44, 4);
+	screenDraw(45, 0, 54, 4);
+	screenDraw(55, 0, 64, 4);
+	screenDraw(65, 0, 74, 4);
+	screenDraw(75, 0, 84, 4);
+	screenDraw(85, 0, 94, 4);
+	screenDraw(95, 0, 104, 4);
+	screenDraw(105, 0, 114, 4);
+	screenDraw(115, 0, 124, 4);
+	screenDraw(125, 0, 134, 4);
+	screenDraw(15, 4, 24, 8);
+	screenDraw(25, 4, 34, 8);
+	screenDraw(35, 4, 44, 8);
+	screenDraw(45, 4, 54, 8);
+	screenDraw(55, 4, 64, 8);
+	screenDraw(65, 4, 74, 8);
+	screenDraw(75, 4, 84, 8);
+	screenDraw(85, 4, 94, 8);
+	screenDraw(95, 4, 104, 8);
+	screenDraw(105, 4, 114, 8);
+	screenDraw(115, 4, 124, 8);
+	screenDraw(125, 4, 134, 8);
+	screenDraw(15, 8, 24, 12);
+	screenDraw(25, 8, 34, 12);
+	screenDraw(35, 8, 44, 12);
+	screenDraw(45, 8, 54, 12);
+	screenDraw(55, 8, 64, 12);
+	screenDraw(65, 8, 74, 12);
+	screenDraw(75, 8, 84, 12);
+	screenDraw(85, 8, 94, 12);
+	screenDraw(95, 8, 104, 12);
+	screenDraw(105, 8, 114, 12);
+	screenDraw(115, 8, 124, 12);
+	screenDraw(125, 8, 134, 12);
+	screenDraw(15, 12, 24, 16);
+	screenDraw(25, 12, 34, 16);
+	screenDraw(35, 12, 44, 16);
+	screenDraw(45, 12, 54, 16);
+	screenDraw(55, 12, 64, 16);
+	screenDraw(65, 12, 74, 16);
+	screenDraw(75, 12, 84, 16);
+	screenDraw(85, 12, 94, 16);
+	screenDraw(95, 12, 104, 16);
+	screenDraw(105, 12, 114, 16);
+	screenDraw(115, 12, 124, 16);
+	screenDraw(125, 12, 134, 16);
+	screenDraw(15, 16, 24, 20);
+	screenDraw(25, 16, 34, 20);
+	screenDraw(35, 16, 44, 20);
+	screenDraw(45, 16, 54, 20);
+	screenDraw(55, 16, 64, 20);
+	screenDraw(65, 16, 74, 20);
+	screenDraw(75, 16, 84, 20);
+	screenDraw(85, 16, 94, 20);
+	screenDraw(95, 16, 104, 20);
+	screenDraw(105, 16, 114, 20);
+	screenDraw(115, 16, 124, 20);
+	screenDraw(125, 16, 134, 20);
+	screenDraw(15, 20, 24, 24);
+	screenDraw(25, 20, 34, 24);
+	screenDraw(35, 20, 44, 24);
+	screenDraw(45, 20, 54, 24);
+	screenDraw(55, 20, 64, 24);
+	screenDraw(65, 20, 74, 24);
+	screenDraw(75, 20, 84, 24);
+	screenDraw(85, 20, 94, 24);
+	screenDraw(95, 20, 104, 24);
+	screenDraw(105, 20, 114, 24);
+	screenDraw(115, 20, 124, 24);
+	screenDraw(125, 20, 134, 24);
+	screenDraw(15, 24, 24, 28);
+	screenDraw(25, 24, 34, 28);
+	screenDraw(35, 24, 44, 28);
+	screenDraw(45, 24, 54, 28);
+	screenDraw(55, 24, 64, 28);
+	screenDraw(65, 24, 74, 28);
+	screenDraw(75, 24, 84, 28);
+	screenDraw(85, 24, 94, 28);
+	screenDraw(95, 24, 104, 28);
+	screenDraw(105, 24, 114, 28);
+	screenDraw(115, 24, 124, 28);
+	screenDraw(125, 24, 134, 28);
+	screenDraw(15, 28, 24, 32);
+	screenDraw(25, 28, 34, 32);
+	screenDraw(35, 28, 44, 32);
+	screenDraw(45, 28, 54, 32);
+	screenDraw(55, 28, 64, 32);
+	screenDraw(65, 28, 74, 32);
+	screenDraw(75, 28, 84, 32);
+	screenDraw(85, 28, 94, 32);
+	screenDraw(95, 28, 104, 32);
+	screenDraw(105, 28, 114, 32);
+	screenDraw(115, 28, 124, 32);
+	screenDraw(125, 28, 134, 32);
+	screenDraw(15, 32, 24, 36);
+	screenDraw(25, 32, 34, 36);
+	screenDraw(35, 32, 44, 36);
+	screenDraw(45, 32, 54, 36);
+	screenDraw(55, 32, 64, 36);
+	screenDraw(65, 32, 74, 36);
+	screenDraw(75, 32, 84, 36);
+	screenDraw(85, 32, 94, 36);
+	screenDraw(95, 32, 104, 36);
+	screenDraw(105, 32, 114, 36);
+	screenDraw(115, 32, 124, 36);
+	screenDraw(125, 32, 134, 36);
 	attroff(COLOR_PAIR(1));
 	attron(COLOR_PAIR(2));
 	move(1, 1);
-	printw("Score: ");
+	printw("Puntuación:");
+	move(3, 1);
+	printw("Tracción:");
+	move(5, 1);
+	printw("Estela Media:");
+	move(7, 1);
+	printw("Estela Alta:");
+	move(9, 1);
+	printw("Invensible:");
+	move(11, 1);
+	printw("Velocidad:");
+	move(1, 136);
+	printw("Puntuación:");
+	move(3, 136);
+	printw("Tracción:");
+	move(5, 136);
+	printw("Estela Media:");
+	move(7, 136);
+	printw("Estela Alta:");
+	move(9, 136);
+	printw("Invensible:");
+	move(11, 136);
+	printw("Velocidad:");
 	attroff(COLOR_PAIR(2));
 	currentY = (maxHeight / 2);
 	currentX = (maxWidth / 2) + 5;
@@ -88,31 +209,31 @@ void moveX() {
 	}
 }
 
-void scoreDraw() {
-	for (int i = 0; i < 9; i++) {
-		move(0, i);
-		addch('_');
+void scoreDraw(int beginX, int beginY, int maxX, int maxY) {
+	for (int i = beginX; i < maxX; i++) {
+		move(beginY, i);
+		addch('*');
 	}
-	for (int i = 0; i < 10; ++i) {
-		move(i, 0);
-		addch('|');
+	for (int i = beginY; i < maxY + 1; ++i) {
+		move(i, beginX);
+		addch('*');
 	}
-	for (int i = 0; i < 9; ++i) {
-		move(10, i);
-		addch('_');
+	for (int i = beginX; i < maxX; ++i) {
+		move(maxY, i);
+		addch('*');
 	}
-	for (int i = 0; i < 10; ++i) {
-		move(i, 9);
-		addch('|');
+	for (int i = beginY; i < maxY + 1; ++i) {
+		move(i, maxX);
+		addch('*');
 	}
 }
 
-void screenDraw(int beginX, int maxX, int maxY) {
+void screenDraw(int beginX, int beginY, int maxX, int maxY) {
 	for (int i = beginX; i < maxX; i++) {
-		move(0, i);
+		move(beginY, i);
 		addch(oldalchar);
 	}
-	for (int i = 0; i < maxY; ++i) {
+	for (int i = beginY; i < maxY; ++i) {
 		move(i, beginX);
 		addch(oldalchar);
 	}
@@ -120,7 +241,7 @@ void screenDraw(int beginX, int maxX, int maxY) {
 		move(maxY, i);
 		addch(oldalchar);
 	}
-	for (int i = 0; i < maxY+1; ++i) {
+	for (int i = beginY; i < maxY + 1; ++i) {
 		move(i, maxX);
 		addch(oldalchar);
 	}
