8faf14fbbbb7043e44e4c6b4deeada67f029008b
howar <howar@howar-HP-G42-Notebook-PC>
2013-03-09 09:24:28 -0600

    cambios

--- git@github.com:lobowhat/proyecto.git/prueba/MainObjects/Thread.h
+++ git@github.com:lobowhat/proyecto.git/prueba/MainObjects/Thread.h
@@ -9,12 +9,16 @@
 #define THREAD_H_
 
 #include <pthread.h>
-
+#include <GUI.h>
+#include <ListQueue.h>
+#include <Matrix.h>
+#include <Player.h>
 class Thread {
 private:
 	pthread_t execute;
 public:
 	Thread();
+
 	virtual ~Thread();
 	static void* functionThread(void* var);
 	void fThread();
